87a8fac0676c39d0113407c88be32ecf3afc91a5
geogrey12muiruri <greggambugua@gmail.com>
2022-12-15 09:46:48 +0300

    G1-4 added the users table with admin attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_044544) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_064142) do
   create_table "caregivers", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -37,6 +37,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_044544) do
     t.integer "parent_id"
     t.integer "caregiver_id"
     t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
