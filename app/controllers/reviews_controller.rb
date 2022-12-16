682c19a7423b6e494661c9f4a79cdc9b86f1faef
calvin <ckodalo@gmail.com>
2022-12-16 10:20:46 +0300

    edits

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+    def index
+        reviews = Review.all
+        render json: reviews
+    end
+    
+    def show
+     
+        review = Review.find_by(id: params[:id])
+        render json: review
+      end
+
+      def create
+        review = Review.create(price: params[:price], pizza_id: params[:pizza_id], restaurant_id: params[:restaurant_id])
+        if review
+            associatedReview = Review.find_by(id: params[:pizza_id])
+            render json: associatedReview
+        else
+            render json: { errors: "validation errors"}, status: :unprocessable_entity
+        
+        end
+      end
+
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -41,13 +41,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_064142) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.boolean "admin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  # create_table "users", force: :cascade do |t|
+  #   t.string "name"
+  #   t.string "email"
+  #   t.string "password_digest"
+  #   t.boolean "admin"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
 
 end
