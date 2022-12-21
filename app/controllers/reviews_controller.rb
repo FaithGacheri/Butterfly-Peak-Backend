b1112c813408da58ef8c66803012abbbe3568751
geogrey12muiruri <greggambugua@gmail.com>
2022-12-21 11:52:08 +0300

    debugging

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,7 @@ class ReviewsController < ApplicationController
         review = Review.all
         render json: review
     end
+
     def create
         review = Review.new(review_params)
         if review.save
@@ -10,10 +11,12 @@ class ReviewsController < ApplicationController
         else
             render json: {error: 'Review not created'}, status: :unprocessable_entity
     end
+
     def show
         review = review.find(params[:id])
         render json: review
     end
+
     def update
         review  = review.find(params[:id])
         review.update(review_params)
@@ -24,8 +27,12 @@ class ReviewsController < ApplicationController
         review.destroy
         render json: review
     end
+    
     private
+   
     def review_params
         params.require(:review).permit(:parent_id, :caregiver_id, :comment)
     end
+end 
+
 end
