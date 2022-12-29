688c0b8e9ad3ef048f7a51800ca0ac5e2d81f806
geogrey12muiruri <greggambugua@gmail.com>
2022-12-29 09:40:05 +0300

    modified the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,24 @@
 # Create fake parents
 10.times do
-    Parent.create(name: Faker::Name.name, email: Faker::Internet.email, address: Faker::Address.street_address, phone: Faker::PhoneNumber.phone_number)
+    Parent.create(firstname: Faker::Name.first_name, last_name: Faker::Name.last_name, username: Faker::Internet.username, email: Faker::Internet.email, password_digest: Faker::Internet.password, phone: Faker::PhoneNumber.phone_number, image: Faker::LoremFlickr.image)
   end
   
   # Create fake caregivers
   10.times do
-    Caregiver.create(name: Faker::Name.name, email: Faker::Internet.email, address: Faker::Address.street_address, phone: Faker::PhoneNumber.phone_number, education: Faker::Educator.degree, experience: Faker::Job.field, is_booked: Faker::Boolean.boolean, age: rand(18..65))
+    Caregiver.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, user_name: Faker::Internet.username, email: Faker::Internet.email, password_digest: Faker::Internet.password, phone: Faker::PhoneNumber.phone_number, avatar: Faker::Lorem)
   end
   
   # Create fake reviews
   20.times do
     Review.create(parent_id: rand(1..10), caregiver_id: rand(1..10), comment: Faker::Lorem.paragraph)
   end
+  10.times do
+    Image.create(caregiver_id: rand(1..10), image: Faker::LoremFlickr.image)
+  end
   
+  10.times do
+    Location.create(parent_id: rand(1..10), caregiver_id: rand(1..10), city: Faker::Address.city, town: Faker::Address.community, street_address: Faker::Address.street_address, province: Faker::Address.state, postal_zipcode: Faker::Address.zip_code)
+  end
   # Create fake admins
   # 5.times do
   #   User.create(name: Faker::Name.name, email: Faker::Internet.email, password_digest: Faker::Internet.password, admin: Faker::Boolean.boolean)
