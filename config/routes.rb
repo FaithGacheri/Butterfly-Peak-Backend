12eb3221f529da9b5b8380ba3f5e05dc399d3a0e
geogrey12muiruri <greggambugua@gmail.com>
2023-01-15 10:47:20 +0300

    minor fixes

--- config/routes.rb
+++ config/routes.rb
@@ -35,26 +35,4 @@ Rails.application.routes.draw do
   resources :locations
   resources :bookings
 
-
-#   post '/authenticate', to: 'authentication#authenticate'
-
-
-# #   post ':slug', to: 'parents#create', constraints: ParentConstraint.new
-# # post ':slug', to: 'caregivers#create'
-# post '/signup/parent', to: 'parents#create'
-# post '/signup/caregiver', to: 'caregivers#create'
-
-# # get "/me", to: "users#show"
-# # post "/login", to: "sessions#create"
-# # delete "/logout", to: "sessions#destroy"
-# post '/parents/signin', to: 'parents_sessions#create'
-# delete '/parents/signout', to: 'parents_sessions#destroy'
-# post '/caregivers/signin', to: 'caregivers_sessions#create'
-# delete '/caregivers/signout', to: 'caregivers_sessions#destroy'
-
-
-
-  
-  
-  
 end
