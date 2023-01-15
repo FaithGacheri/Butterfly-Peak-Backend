9dab27d8303419bfb0c78697085b9632f0d2297c
calvin <ckodalo@gmail.com>
2023-01-16 02:42:40 +0300

    added password routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,13 @@ Rails.application.routes.draw do
   resources :images
   resources :locations
 
+  #password reset routes
+
+  get '/password/reset', to: 'password_resets#new'
+    post '/password/reset', to: 'password_resets#create'
+    get '/password/reset/edit', to: 'password_resets#edit'
+    patch '/password/reset/edit', to: 'password_resets#update'
+
 #   post '/authenticate', to: 'authentication#authenticate'
 
 
