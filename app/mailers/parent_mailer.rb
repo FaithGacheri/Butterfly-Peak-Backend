a215a7bea7cf7278f271ead3ee412b5c579bf8f4
calvin <ckodalo@gmail.com>
2023-01-16 00:50:00 +0300

    added password reset function

--- app/mailers/parent_mailer.rb
+++ app/mailers/parent_mailer.rb
@@ -1,8 +1,17 @@
 class ParentMailer < ApplicationMailer
-    def forgot_password
-        @parent = params[:parent]
-        #@user = params[:user]
-        #@url  = 'http://example.com/login'
-        mail(to: @parent.email, subject: 'Welcome to My Awesome Site')
-      end
+    # def forgot_password
+    #     @parent = params[:parent]
+    #     #@user = params[:user]
+    #     #@url  = 'http://example.com/login'
+    #     mail(to: @parent.email, subject: 'Welcome to My Awesome Site')
+    #   end
+
+    def reset
+      @parent = params[:parent]
+      @token = @parent.signed_id(purpose: 'password_reset', expires_in: 15.minutes)
+  
+      mail to: @parent.email_address
+    end
+
+
 end
