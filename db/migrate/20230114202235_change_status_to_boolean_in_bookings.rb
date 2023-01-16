299744624d1239314e8cb24c3189209211e6095d
geogrey12muiruri <greggambugua@gmail.com>
2023-01-17 00:50:54 +0300

    minor changes

--- db/migrate/20230114202235_change_status_to_boolean_in_bookings.rb
+++
@@ -1,6 +0,0 @@
-class ChangeStatusToBooleanInBookings < ActiveRecord::Migration[7.0]
-  def change
-    change_column :bookings, :status, :boolean
-    change_column_default :bookings, :status, false
-  end
-end
--- db/migrate/20230116195951_update_booking_status.rb
+++
@@ -1,5 +0,0 @@
-class UpdateBookingStatus < ActiveRecord::Migration[7.0]
-  def change
-    change_column :bookings, :status, :string, default: 'pending'
-  end
-end
