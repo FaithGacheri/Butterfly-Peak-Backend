71da08ef86cda09d9caaaf2568caef8f71c05979
calvin <ckodalo@gmail.com>
2023-01-20 09:18:19 +0300

    edit

--- db/migrate/20230117075117_update_bookings_status_to_boolean.rb
+++ db/migrate/20230117075117_update_bookings_status_to_boolean.rb
@@ -1,5 +1,5 @@
 class UpdateBookingsStatusToBoolean < ActiveRecord::Migration[7.0]
   def change
-    change_column :bookings, :status, :boolean, using: 'status::boolean'
+    change_column :bookings, :status, :boolean, using: 'status::boolean', default: false
   end
 end
